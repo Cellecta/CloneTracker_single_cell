73d0e2adf6ed72ddcb3e2d3b9adfa6cca9847b1a
Dongfang Hu <dfhu@cellecta.com>
2026-03-19 16:05:16 -0700

    Remove project-specific sample CSVs and QC helper scripts

--- control_qc_drugB.sh
+++
@@ -1,6 +0,0 @@
-python scRNAseq_QC.py \
-    --input /mnt/project/Cellecta_scCRISPR/103951_Revmed/drugB_GEX/outs/ \
-    --output /mnt/project/Cellecta_scCRISPR/103951_Revmed/QC/drugB \
-    --sample-name drugB \
-    --clonetracker-summary /mnt/project/Cellecta_scCRISPR/103951_Revmed/CloneTracker_out_combined/drugB/drugB_barcode_assignment_summary_1.tsv \
-    --clonetracker-umi /mnt/project/Cellecta_scCRISPR/103951_Revmed/CloneTracker_out_combined/drugB/cell_clonetracker_barcode_table.tsv
